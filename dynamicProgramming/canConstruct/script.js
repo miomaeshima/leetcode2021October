890dfdcafb2b0c0cf437eea4a3f419d3ef6139f8
miomaeshima <miomamomo@gmail.com>
2021-11-01 07:34:47 +0900

    memoized canConstruct

--- dynamicProgramming/canConstruct/script.js
+++ dynamicProgramming/canConstruct/script.js
@@ -2,38 +2,49 @@
 // and checks if the string can be constructed with element(s) in the array
 // It returns a boolean
 
-// const canConstruct = (targetString, wordBank, result = false) => {
-//   if (targetString.length === 0) return true;
-//   // if the word and the first part of targetString are not the same, return false;
+// if string is empty, return true
+// if word is prefix of string, reset string by removing prefix and recurse
 
-//   //check if word and the first part of targetString are the same, if so, remove the word from the targetString and continue to do recursion
-//   for (let word in wordBank) {
-//     if (targetString.slice(0, word.length) === word) {
+////Brute Force:
+// const canConstruct = (targetString, wordBank) => {
+//   if (targetString === "") return true;
+//   for (let word of wordBank) {
+//     if (targetString.indexOf(word) === true) {
 //       targetString = targetString.slice(word.length);
-//       let newTargetStringStartsWithWord = canConstruct(targetString, wordBank, result);
-//       result = (result || newTargetStringStartsWithWord);
-//     } else {
-//       return false;
+//       if (canConstruct(targetString, wordBank) === true) {
+//         return true;
+//       }
 //     }
 //   }
-//   return result;
+//   return false;
 // };
 
-const canConstruct = (targetString, wordBank) => {
+const canConstruct = (targetString, wordBank, memo = {}) => {
+  if (targetString in memo) return memo[targetString];
   if (targetString === "") return true;
   for (let word of wordBank) {
     if (targetString.indexOf(word) === true) {
-      targetString = targetString.slice(word.length);
-      if (canConstruct(targetString, wordBank) === true) {
+      suffix = targetString.slice(word.length);
+      if (canConstruct(suffix, wordBank, memo) === true) {
+        memo[targetString] = true;
         return true;
       }
     }
+    memo[targetString] = false;
+    return false;
   }
-  return false;
 };
 
 console.log(canConstruct("abcdef", ["ab", "def"]));
 console.log(canConstruct("abcdef", ["cde", "abc", "f"]));
+console.log(
+  canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "e",
+    "eee",
+    "eeeee",
+    "eeeeeee",
+  ])
+);
 
 console.log("potato".indexOf("pq")); //-1
 console.log("potato".indexOf("pot")); //0
